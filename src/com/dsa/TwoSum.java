6c9c5393d98262dc99b17cfdb2a327158a385bfe
unknown <nikhilpatil6700>
2025-02-10 21:43:13 +0530

    2nd day

--- src/com/dsa/TwoSum.java
+++ src/com/dsa/TwoSum.java
@@ -7,22 +7,22 @@ public class TwoSum {
 		int[] arr= {1,2,3,4};
 		int target=5;
 		twoSum(arr, target);
+		
 	}
 	
 	public static void twoSum(int[] arr,int target) {
 		
 		for(int i=0;i<arr.length;i++) {
-			boolean flag=false;
+			//boolean flag=false;
 			for(int j=i+1;j<arr.length;j++) {
 				if(arr[i]+arr[j]==target) {
 					System.out.println(arr[i]+" "+arr[j]);
 					break;
-					
-					
+						
 				}
 		}
 	}
-		
-
-	}
+ }
+	
+	
 }
